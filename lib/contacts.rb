25e86d2e75c29b9dcd5d92c126ce256ddc9752e4
tmharkins <tmharkins@gmail.com>
2016-02-19 22:11:29 -0600

    iterating through nested hash

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -1,4 +1,4 @@
-require 'pry'
+require 'pry' 
 
   # This is the array we will be passing into the remove_strawberry method
   # contacts = {
@@ -15,5 +15,11 @@ require 'pry'
   # }
   
 def remove_strawberry(contacts)
-
+  contacts.each do |person, contact_details_hash|
+    contact_details_hash.each do |attribute, data|
+      if attribute == :favorite_icecream_flavors
+        data.delete_if {|ice_cream| ice_cream == "strawberry"}
+      end
+    end
+  end
 end
